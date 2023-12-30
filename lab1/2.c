3fbcf1b3e3cee1177484b73b9fed8549141028c6
mohamedishhaq <mimishaq010@gmail.com>
2023-12-30 12:24:28 +0500

    Question 2 add

--- lab1/2.c
+++ lab1/2.c
@@ -1,4 +1,4 @@
-/*Write a C program that takes the height and width of a rectangle as an input from user 
+/*2.Write a C program that takes the height and width of a rectangle as an input from user 
 and compute the perimeter and area of a rectangle.
 */
 
